ee678d5516d5372edcba4eeb99be231386f9d88c
edson <edson.cs.dev@gmail.com>
2021-08-24 18:50:24 -0300

    drop database endpoint

--- application/routes/api.py
+++ application/routes/api.py
@@ -28,6 +28,15 @@ def init_app(app, access_point="/api", encoding='utf-8'):
           table = repository[name]
       return list(table.all())
 
+  @app.get(access_point + "/{name}/delete_table", tags=[access_point])
+  async def delete_table(name: str):
+      # define table attributes
+      if repository[name].exists:
+          table = repository[name]
+          table.drop()
+      return {"success": "true"}
+
+
   @app.get(access_point + "/{name}", tags=[access_point])
   async def get_itens(name: str):
       table = repository[name]
